4caa5657a4f179f8c887341925294281350f2c66
Sabbir Hossain Shihab <sabbirhossainrinku@gmail.com>
2024-09-30 00:36:40 +0600

    Update 01 30.09.2024 FROM HOME

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -7,25 +7,24 @@ const LatestCollection = () => {
     const { products } = useContext(ShopContext);
     const [latestProducts, setLatestProducts] = useState([]);
 
-    useEffect(()=>{
-        setLatestProducts(products.slice(0,10));
-    },[products])
+    useEffect(() => {
+        setLatestProducts(products.slice(0, 10));
+    }, [products])
     return (
         <div className='my-10'>
             <div className="text-center py-8 text-3xl">
-                <Title text1={'LATEST'} text2={'COLLECTION'}/>
+                <Title text1={'LATEST'} text2={'COLLECTION'} />
                 <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo voluptate sed omnis fuga perferendis fugiat at expedita esse facere? Nostrum dolor doloremque, ullam aspernatur omnis in veniam assumenda est fugit!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo voluptate sed omnis fuga perferendis fugiat at expedita esse facere? Nostrum dolor doloremque, ullam aspernatur omnis in veniam assumenda est fugit!
                 </p>
             </div>
 
-        {/* Rendering Products */}
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-            {latestProducts.map((item,index)=>(
-                <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
-            ))}
-
-        </div>
+            {/* Rendering Products */}
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+                {latestProducts.map((item, index) => (
+                    <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+                ))}
+            </div>
 
         </div>
     )
