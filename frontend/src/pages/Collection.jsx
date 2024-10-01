3e07ffc01ef239a283d7398f1f296de2c7f408e6
Sabbir Hossain Shihab <sabbirhossainrinku@gmail.com>
2024-10-02 00:14:26 +0600

    Update 02 01.10.2024 FROM HOME

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -6,15 +6,15 @@ import ProductItem from '../components/ProductItem'
 
 const Collection = () => {
 
-    const { products , search, showSearch} = useContext(ShopContext);
+    const { products, search, showSearch } = useContext(ShopContext);
     const [showFilter, setShowFilter] = useState(false);
     const [filterProducts, setFilterProducts] = useState([]);
     const [category, setCategory] = useState([]);
     const [subCategory, setSubCategory] = useState([]);
     const [sortType, setSortType] = useState('relevant');
 
-    const toggleCategory = (e) =>{
-        if(category.includes(e.target.value)){  
+    const toggleCategory = (e) => {
+        if (category.includes(e.target.value)) {
             setCategory(prev => prev.filter(item => item !== e.target.value))
         }
         else {
@@ -22,25 +22,25 @@ const Collection = () => {
         }
     }
 
-    const toggleSubcategory = (e) =>{
-        if(subCategory.includes(e.target.value)){
+    const toggleSubcategory = (e) => {
+        if (subCategory.includes(e.target.value)) {
             setSubCategory(prev => prev.filter(item => item !== e.target.value))
         }
-        else{
+        else {
             setSubCategory(prev => [...prev, e.target.value])
         }
     }
 
     const applyFilter = () => {
         let productsCopy = products.slice();
-        if(showSearch && search){
+        if (showSearch && search) {
             productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
         }
-        if(category.length > 0){
+        if (category.length > 0) {
             productsCopy = productsCopy.filter(item => category.includes(item.category))
         }
 
-        if(subCategory.length >0 ){
+        if (subCategory.length > 0) {
             productsCopy = productsCopy.filter(item => subCategory.includes(item.subCategory))
         }
 
@@ -50,12 +50,12 @@ const Collection = () => {
     const sortProduct = () => {
         let fpCopy = filterProducts.slice();
 
-        switch(sortType){
+        switch (sortType) {
             case 'low-high':
-                setFilterProducts(fpCopy.sort((a,b)=>(a.price - b.price)));
+                setFilterProducts(fpCopy.sort((a, b) => (a.price - b.price)));
                 break;
             case 'high-low':
-                setFilterProducts(fpCopy.sort((a,b)=>(b.price - a.price)));
+                setFilterProducts(fpCopy.sort((a, b) => (b.price - a.price)));
                 break;
             default:
                 applyFilter();
@@ -71,12 +71,12 @@ const Collection = () => {
     //     console.log(subCategory)
     // },[subCategory])
 
-    useEffect(()=>{
+    useEffect(() => {
         applyFilter();
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[category,subCategory,search,showSearch])
+    }, [category, subCategory, search, showSearch])
 
-    useEffect(()=>{
+    useEffect(() => {
         sortProduct();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [sortType])
@@ -124,14 +124,14 @@ const Collection = () => {
             <div className='flex-1'>
                 <div className="flex justify-between text-base sm:text-2xl mb-4">
                     <Title text1={'ALL'} text2={'COLLECTION'} />
-                    <select onChange={(e)=>setSortType(e.target.value)} className='border-2 border-gray-300 px-2 text-sm'>
+                    <select onChange={(e) => setSortType(e.target.value)} className='border-2 border-gray-300 px-2 text-sm'>
                         <option value="relevant">Sort by: Relevant</option>
                         <option value="low-high">Sort by: Low to High</option>
                         <option value="high-low">Sort by: High to Low</option>
                     </select>
                 </div>
                 <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6">
-                    {filterProducts.map((item,index)=>(
+                    {filterProducts.map((item, index) => (
                         <ProductItem key={index} name={item.name} id={item._id} price={item.price} image={item.image} />
                     ))}
                 </div>
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,17 +2,19 @@ import { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
 
-    const {productId} = useParams();
-    const {products, currency } = useContext(ShopContext);
+    const { productId } = useParams();
+    const { products, currency } = useContext(ShopContext);
     const [productData, setProductData] = useState(false);
-    const [image,setImage] = useState('');
+    const [image, setImage] = useState('');
+    const [size, setSize] = useState('');
 
-    const fetchProductData = async() =>{
-        products.map((item)=>{
-            if (item._id === productId){
+    const fetchProductData = async () => {
+        products.map((item) => {
+            if (item._id === productId) {
                 setProductData(item);
                 setImage(item.image[0]);
                 return null;
@@ -21,7 +23,7 @@ const Product = () => {
 
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchProductData();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [productId, products])
@@ -34,8 +36,8 @@ const Product = () => {
                 <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
                     <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
                         {
-                            productData.image.map((item,index)=>(
-                                <img onClick={()=>setImage(item)} src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt="" /> 
+                            productData.image.map((item, index) => (
+                                <img onClick={() => setImage(item)} src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt="" />
                             ))
                         }
                     </div>
@@ -56,8 +58,38 @@ const Product = () => {
                     </div>
                     <p className='text-3xl mt-5 font-medium'>{currency}{productData.price}</p>
                     <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+                    <div className='flex flex-col gap-4 my-8'>
+                        <p>Select Size</p>
+                        <div className='flex gap-2'>
+                            {productData.sizes.map((item, index) => (
+                                <button onClick={() => setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`} key={index}>{item}</button>
+                            ))}
+
+                        </div>
+                    </div>
+                    <button className='bg-black text-white py-3 px-8 text-sm active:bg-gray-700'>ADD TO CART</button>
+                    <hr className='mt-8 sm:w-4/5' />
+                    <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+                        <p>100% Original product</p>
+                        <p>Cash on delivery is available on this product</p>
+                        <p>Easy return and exchange policy within 7 days</p>
+                    </div>
                 </div>
             </div>
+            {/* Description and Review section */}
+            <div className='mt-5'>
+                <div className='flex'>
+                    <b className='border px-5 py-3 text-sm'>Description</b>
+                    <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+                </div>
+                <div className='flex flex-col border gap-4 px-6 py-6 text-gray-500 text-sm'>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quidem illo error quae quam illum laboriosam temporibus, praesentium quia veritatis nemo optio ducimus distinctio consequuntur voluptate eaque voluptates laudantium perspiciatis.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse consequuntur corrupti culpa vel eaque! Eaque exercitationem minima eius, ullam modi labore iure. Corrupti quidem facere ad, sit ipsa neque placeat!</p>
+                </div>
+            </div>
+
+            {/* Related Products */}
+            <RelatedProducts category={productData.category} subCategory={productData.subCategory} />
         </div>
     ) : <div className='opacity-0'></div>
 }
